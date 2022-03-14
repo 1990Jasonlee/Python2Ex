3fecb4b548da01514373fd707c5fcb82cac5152c
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 15:08:59 -0400

    update

--- greet.py
+++ greet.py
@@ -1,8 +1,8 @@
 def greet(name):
-    print(f"Hello {name}")
+    print(f'Hello {name}')
 
 def name_input():
-    input_name = input("Enter a name:")
+    input_name = input('Enter a name:')
     greet(input_name)
 
 name_input()
